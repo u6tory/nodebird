a9f5b1626003d7ca9c790a21b457bca398295a95
root <u6tory@gmail.com>
2020-08-11 16:04:45 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import AppLayout from "../components/AppLayout";
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>Welcome Home</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
